9a9adf959baaca1e6abed9b91bc0272ce61dc160
Zlatin Stanimirov <zlatin_stanimirovv@abv.bg>
2013-11-21 17:12:36 +0200

    Added the javascript logic to the editor

--- SPS/modals.js
+++ SPS/modals.js
@@ -22,3 +22,51 @@
             });
         });
 		
+/* Modal for the text insertion */
+	//modal initialization
+ $(function () {
+         $("#dialog-confirm1").dialog({
+             autoOpen: false,
+             resizable: true,
+             height: 680,
+             width: 500,
+             modal: true,
+             buttons: {
+                 "Confirm": function () {
+                     getFormatedText();
+                     $(this).dialog("close");
+                 },
+                 Cancel: function () {
+                     $(this).dialog("close");
+                 }
+             }
+         });     
+     });
+
+        var textGenId = "#textGen";
+        var textGenPrevId = "#textGenPreview";
+        window.setInterval(function () { updatePreview(); }, 350);
+
+		//Updates the preview of the text
+        function updatePreview() {
+            console.log($(textGenId).val());
+            $(textGenPrevId).html($(textGenId).val());
+        }
+		
+		// Depending on the parameter Creates a new tag
+		// at cursor position
+        function textbox2(tag) {
+            var cursorPos = $(textGenId).prop('selectionStart');
+            var v = $(textGenId).val();
+            var textBefore = v.substring(0, cursorPos);
+            var textAfter = v.substring(cursorPos, v.length);
+            $(textGenId).val(textBefore + tag + textAfter);
+        }
+		
+		// Sets the formated text to the selected div
+        function getFormatedText() {
+            var tmpStr = $(textGenId).val();
+            console.log(tmpStr);
+            document.getElementById(currID).innerHTML = tmpStr;
+            $(textGenId).val("");
+        }
